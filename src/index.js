2ab53427e011d5c9fdbdbfb6a2482dbed0fe1ef4
AndresMontealegre <55224609+AndresMontealegre@users.noreply.github.com>
2021-03-01 19:59:21 -0500

    Andres Montealegre 1825747

--- src/index.js
+++ src/index.js
@@ -1,50 +1,8 @@
-//Ajustar texto
-//juan camilo gonzalez
-console.log('hola 1')
+import React from 'react'
+import ReactDom from 'react-dom'
 
-const name = 'Fredy';
-const lastname = 'Ballesteros';
+import PrimerComponente from './PrimerComponente'
 
-let inputdata = 8;
-inputdata = 4;
+const divRoot = document.querySelector('#root')
 
-console.log(name, lastname, inputdata)
-
-if (true){
-    let inputdata = 78
-    console.log(inputdata)
-}
-console.log(inputdata)
-
-const fullname = `${name} ${lastname}`
-console.log(fullname)
-// Fredy Ballesteros
-//Desarrollo SW2
-
-let i = 0
-i++
-//cesar solano
-
-
-const person ={
-    name: 'Fredy',
-    lastname: 'Ballesteros',
-    age:43
-}
-
-console.table(person);
-const person1 ={
-    name: 'Fredy',
-     lastname: 'Ballesteros',
-     age:43,
-     address: {
-        city: 'Cali',
-        zip: 6544,
-        lat: 14.24,
-        ing: 34.52
-     }
-}
-
-console.log(person1)
-const person3 = { ...person1}
-console.log(person3)
+ReactDom.render(<PrimerComponente propNum="0"/>, divRoot)
