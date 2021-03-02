b698668cfb604c9b750a1084c453b0ea7c2fa4ad
Victor <victor.993_@hotmail.com>
2021-03-01 19:55:14 -0500

    contador Victor Manuel

--- src/index.js
+++ src/index.js
@@ -1,50 +1,7 @@
-//Ajustar texto
-//juan camilo gonzalez
-console.log('hola 1')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Contador from './contador'
 
-const name = 'Fredy';
-const lastname = 'Ballesteros';
+const divroot = document.querySelector('#root')
 
-let inputdata = 8;
-inputdata = 4;
-
-console.log(name, lastname, inputdata)
-
-if (true){
-    let inputdata = 78
-    console.log(inputdata)
-}
-console.log(inputdata)
-
-const fullname = `${name} ${lastname}`
-console.log(fullname)
-// Fredy Ballesteros
-//Desarrollo SW2
-
-let i = 0
-i++
-//cesar solano
-
-
-const person ={
-    name: 'Fredy',
-    lastname: 'Ballesteros',
-    age:43
-}
-
-console.table(person);
-const person1 ={
-    name: 'Fredy',
-     lastname: 'Ballesteros',
-     age:43,
-     address: {
-        city: 'Cali',
-        zip: 6544,
-        lat: 14.24,
-        ing: 34.52
-     }
-}
-
-console.log(person1)
-const person3 = { ...person1}
-console.log(person3)
+ReactDOM.render(<Contador time={0} />, divroot)
