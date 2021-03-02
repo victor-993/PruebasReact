1b5220356e2d66a01c1dfcaa1195e825fe2554d2
DavidJoan53 <davidjoan53@gmail.com>
2021-03-01 19:59:41 -0500

    Trabajo en clase - Joan S

--- src/index.js
+++ src/index.js
@@ -1,50 +1,9 @@
-//Ajustar texto
-//juan camilo gonzalez
-console.log('hola 1')
+//Joan Suarez - 1828989
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const name = 'Fredy';
-const lastname = 'Ballesteros';
+import Contador from './Contador'
 
-let inputdata = 8;
-inputdata = 4;
+const divRoot = document.querySelector('#root')
 
-console.log(name, lastname, inputdata)
-
-if (true){
-    let inputdata = 78
-    console.log(inputdata)
-}
-console.log(inputdata)
-
-const fullname = `${name} ${lastname}`
-console.log(fullname)
-// Fredy Ballesteros
-//Desarrollo SW2
-
-let i = 0
-i++
-//cesar solano
-
-
-const person ={
-    name: 'Fredy',
-    lastname: 'Ballesteros',
-    age:43
-}
-
-console.table(person);
-const person1 ={
-    name: 'Fredy',
-     lastname: 'Ballesteros',
-     age:43,
-     address: {
-        city: 'Cali',
-        zip: 6544,
-        lat: 14.24,
-        ing: 34.52
-     }
-}
-
-console.log(person1)
-const person3 = { ...person1}
-console.log(person3)
+ReactDOM.render(<Contador acumulado />, divRoot)
